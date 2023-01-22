9f1708d067bd725bda2d1d99c1a16edc0b136cb3
wwj <2351274018@qq.com>
2023-01-22 11:00:35 +0800

    system模块

---
+++ xuecheng-plus-generator/src/main/java/com/xuecheng/generator/content/controller/TeachplanController.java
@@ -0,0 +1,25 @@
+package com.xuecheng.generator.content.controller;
+
+import com.xuecheng.generator.content.service.TeachplanService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+
+/**
+ * <p>
+ * 课程计划 前端控制器
+ * </p>
+ *
+ * @author itcast
+ */
+@Slf4j
+@RestController
+@RequestMapping("teachplan")
+public class TeachplanController {
+
+    @Autowired
+    private TeachplanService teachplanService;
+}
---
+++ xuecheng-plus-generator/src/main/java/com/xuecheng/generator/content/service/CourseTeacherService.java
@@ -0,0 +1,16 @@
+package com.xuecheng.generator.content.service;
+
+import com.xuecheng.generator.content.model.po.CourseTeacher;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * <p>
+ * 课程-教师关系表 服务类
+ * </p>
+ *
+ * @author itcast
+ * @since 2022-12-10
+ */
+public interface CourseTeacherService extends IService<CourseTeacher> {
+
+}
